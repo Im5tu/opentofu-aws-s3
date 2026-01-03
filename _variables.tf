f0401b77f5fe7c0fe09757001f7f6045f1296093
Stuart Blackler <Im5tu@users.noreply.github.com>
2026-01-03 22:25:44 +0000

    Initial s3 module (#1)

--- _variables.tf
+++ _variables.tf
@@ -1,7 +1,52 @@
-# TODO: Add your module variables here
+variable "name" {
+  type        = string
+  description = "The name of the S3 bucket"
+}
 
-variable "tags" {
-  description = "Tags to apply to resources"
-  type        = map(string)
-  default     = {}
+variable "bucket_acl" {
+  type        = string
+  description = "The ACL for the bucket. Default: private"
+  default     = "private"
+}
+
+variable "bucket_policy" {
+  type        = string
+  description = "The policy that will be applied to the bucket"
+  default     = null
+}
+
+variable "enable_versioning" {
+  type        = bool
+  description = "Whether or not to enable versioning on bucket contents"
+  default     = false
+}
+
+variable "enable_lifecycle_policy" {
+  type        = bool
+  description = "Whether or not to enable lifecycle on bucket contents"
+  default     = true
+}
+
+variable "kms_master_key_id" {
+  type        = string
+  description = "The AWS KMS master key ID used for server-side encryption. If null, AES256 encryption is used."
+  default     = null
+}
+
+variable "enable_logging" {
+  type        = bool
+  description = "Whether to enable access logging for the bucket"
+  default     = false
+}
+
+variable "logging_target_bucket" {
+  type        = string
+  description = "The name of the bucket to store access logs"
+  default     = null
+}
+
+variable "logging_target_prefix" {
+  type        = string
+  description = "The prefix for access log objects"
+  default     = "logs/"
 }
--- main.tf
+++ main.tf
@@ -1,1 +1,73 @@
-# TODO: Add your module resources here
+resource "aws_s3_bucket" "this" {
+  bucket = var.name
+
+  tags = {
+    Name = var.name
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  count  = var.bucket_policy == null ? 0 : 1
+  bucket = aws_s3_bucket.this.bucket
+  policy = var.bucket_policy
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  count  = var.bucket_acl == "private" ? 0 : 1
+  bucket = aws_s3_bucket.this.bucket
+  acl    = var.bucket_acl
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  count  = var.bucket_acl == "private" ? 1 : 0
+  bucket = aws_s3_bucket.this.bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  count  = var.enable_versioning ? 1 : 0
+  bucket = aws_s3_bucket.this.bucket
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
+  count  = var.enable_lifecycle_policy ? 1 : 0
+  bucket = aws_s3_bucket.this.bucket
+
+  rule {
+    id     = "log"
+    status = "Enabled"
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = var.kms_master_key_id != null ? "aws:kms" : "AES256"
+      kms_master_key_id = var.kms_master_key_id
+    }
+  }
+}
+
+resource "aws_s3_bucket_logging" "this" {
+  count  = var.enable_logging && var.logging_target_bucket != null ? 1 : 0
+  bucket = aws_s3_bucket.this.bucket
+
+  target_bucket = var.logging_target_bucket
+  target_prefix = var.logging_target_prefix
+}
